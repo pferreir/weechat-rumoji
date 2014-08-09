1afce8ff965dbf2e5dcbd494c8ab3111c9c8a34b
Pedro Ferreira <pedro@dete.st>
2014-08-09 19:05:56 +0200

    Added main source file

---
+++ rumoji.rb
@@ -0,0 +1,17 @@
+require "rumoji/emoji"
+
+def render(data, modifier, modifier_data, message)
+  message.sub(/(:[\w-]+:)/) do |full|
+    sym = Rumoji::Emoji.find(full[1..-2].intern)
+    sym ? sym.to_s : full
+  end
+end
+
+def weechat_init
+  Weechat.register 'rumoji', 'Pedro Ferreira', '0.0.1', 'MIT',
+    'Render emoji using Rumoji', '', ''
+
+  Weechat.hook_modifier("weechat_print", "render", "")
+
+  Weechat::WEECHAT_RC_OK
+end
